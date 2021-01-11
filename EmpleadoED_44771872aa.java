02d9aaf44d178574a83113bbf8c2b77b79cfc5e3
diseñadora_PAULA <pedromartinezz@gmail.com>
2021-01-11 17:13:58 +0100

    codigo java sin javadoc

---
+++ EmpleadoED_44771872aa.java
@@ -0,0 +1,33 @@
+package empleadoED_44654545aa;
+
+import java.util.*;
+
+
+public class EmpleadoED_44771872AA {
+
+    static final double FACTOR_HORA_EXTRA = 1.2;
+    private String nombreCompleto, dni;
+    private double salarioBasePorHora = 10.56;
+
+    public EmpleadoED_44771872AA(String nombreCompleto, String dni) {
+        this.nombreCompleto = nombreCompleto;
+        this.dni = dni;
+    }
+
+    public double getSalarioEsteMes(int horasExtra) {
+        double cantidadExtra, salarioFinal;
+        cantidadExtra = horasExtra * this.salarioBasePorHora * FACTOR_HORA_EXTRA;
+        salarioFinal = this.salarioBasePorHora * 40 * 4 + cantidadExtra;
+        salarioFinal = (double) Math.round(salarioFinal * 100d) / 100d;
+        return salarioFinal;
+    }
+
+    // Este método no lo comentaremos con JavaDoc
+    public static void main(String[] args) {
+        // !!!! MODIFICA TU NOMBRE Y TU DNI EN LA SIGUIENTE LÍNEA
+        EmpleadoED_44771872AA emp = new EmpleadoED_44771872AA("SERGIO BADAL", "44654545X");
+        int horasExtra = 2;
+        System.out.println("El salario de " + emp.nombreCompleto + " este mes será de " + emp.getSalarioEsteMes(horasExtra)
+                + " euros, al hacer " + horasExtra + " horas extra.");
+    }
+}
